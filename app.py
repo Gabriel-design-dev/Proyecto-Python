dfc15965f1d7bdce535db7098ae717c1a49b654f
Gabriel <66288599+Gabriel-design-dev@users.noreply.github.com>
2023-08-12 23:08:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,18 @@ def crear_registro():
     return render_template("index.html",mensaje2="Usuario Registrado Exitosamente")
 #-----------------------------------
 
+#-----LISTAR USUARIOS-------------
+@app.route('/listar', methods= ["GET", "POST"])
+def listar(): 
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM usuarios")
+    usuarios = cur.fetchall()
+    cur.close()
+    
+    return render_template("listar_usuarios.html",usuarios=usuarios)
 
 
+#----------------------------------
 
 if __name__ == '__main__':
    app.secret_key = "pinchellave"
